8621eef702beb4136bc8549fa8fca064caacb66f
Bart Trzynadlowski <trzy@uw.edu>
2020-07-19 20:42:08 -0700

    Initial commit

---
+++ Quest-XRNode-Test/Assets/Test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+public class Test : MonoBehaviour
+{
+  // Update is called once per frame
+  void Update()
+  {
+    InputDevice device = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+
+    if (!device.isValid)
+    {
+      Debug.Log("XR device is invalid");
+    }
+
+    if (device.TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 devicePos))
+    {
+      Debug.LogFormat("DEVICE POS={0}", devicePos);
+    }
+
+    if (device.TryGetFeatureValue(CommonUsages.leftEyePosition, out Vector3 leftEyePos))
+    {
+      Debug.LogFormat("EYE POS={0}", leftEyePos);
+    }
+  }
+}
+  
